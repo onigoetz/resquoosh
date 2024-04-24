fa8165fbae08aed3895cadb8eb2d5da227756053
Stéphane Goetz <stephane.goetz@onigoetz.ch>
2024-04-24 21:49:06 +0200

    Add some exclusions to biome

--- src/emscripten-types.d.ts
+++ src/emscripten-types.d.ts
@@ -20,9 +20,9 @@ declare namespace EmscriptenWasm {
 		printErr(str: string): void;
 		arguments: string[];
 		environment: EnvironmentType;
-		preInit: () => void[];
-		preRun: () => void[];
-		postRun: () => void[];
+		preInit: (() => void)[];
+		preRun: (() => void)[];
+		postRun: (() => void)[];
 		preinitializedWebGLContext: WebGLRenderingContext;
 		noInitialRun: boolean;
 		noExitRuntime: boolean;
@@ -125,6 +125,6 @@ declare namespace EmscriptenWasm {
 		_free(ptr: number): void;
 
 		// Augmentations below by @surma.
-		onRuntimeInitialized: () => void | null;
+		onRuntimeInitialized: () => null;
 	}
 }
